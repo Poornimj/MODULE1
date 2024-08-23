fa54876404b1df77c898e3e56eb23886d41208f4
Poornimj <poornimj@metropolia.fi>
2024-08-23 22:48:46 +0530

    test

---
+++ Exercise 3.py
@@ -0,0 +1,8 @@
+#Exercise3
+import math
+rec_length = float(input("What is the length of the rectangle? "))
+rec_width = float(input("What is the width of the rectangle? "))
+peri_rec = 2*rec_length + 2*rec_width
+area_rec = rec_length*rec_width
+print(f"The perimeter of the rectangle is: {peri_rec:.1f}")
+print(f"The perimeter of the rectangle is: {area_rec:.1f}")
