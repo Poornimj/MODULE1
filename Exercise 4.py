567d786681bbcc0355d0b1ab3b2b82ba842efda8
Poornimj <poornimj@metropolia.fi>
2024-08-23 22:59:50 +0530

    test

---
+++ Exercise 4.py
@@ -0,0 +1,7 @@
+# Exercise 4
+import math
+num1 = int(input("Give three integer numbers:"))
+num2 = int(input())
+num3 = int(input())
+print(f"Sum of 3 numbers is {num1+num2+num3}, product {num1*num2*num3}, average {(num1+num2+num3)/3}")
+
